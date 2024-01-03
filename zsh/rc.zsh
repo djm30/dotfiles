abed9822d2e66a80aa76d043c12270c76bc8041e
Dylan Morrison <74678796+djm30@users.noreply.github.com>
2024-01-03 08:23:13 +0000

    Update rc.zsh

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -7,26 +7,20 @@ source_if_exists () {
 # Source the environment-specific configurations
 source_if_exists $HOME/.env.sh
 
-# Powerlevel10k instant prompt initialization
 source_if_exists "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 
-# Oh-My-Zsh and theme
+# Oh-My-Zsh
 export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="powerlevel10k/powerlevel10k"
 source_if_exists $ZSH/oh-my-zsh.sh
 
-# Custom configurations from DOTFILES
 source_if_exists $DOTFILES/zsh/aliases.zsh
 source_if_exists $DOTFILES/zsh/commands.zsh
 
-# Additional configurations
 source_if_exists ~/.fzf.zsh
 source_if_exists /opt/homebrew/etc/profile.d/z.sh
 
-# Powerlevel10k configuration
 source_if_exists ~/.p10k.zsh
 
-# Environment-specific PATH configurations
 export PATH="$PATH:/usr/local/sbin:$DOTFILES/bin:$HOME/.local/bin"
 export VISUAL=nvim
 export EDITOR=nvim
@@ -34,4 +28,6 @@ export TERM=wezterm
 
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme
+ZSH_THEME="powerlevel10k/powerlevel10k"
 # plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
